f1736348617a839919b331d7813ab78b514a93f7
froggy <chrisdetmering@gmail.com>
2019-07-17 10:31:56 -0700

    Fixed valid_moves and built build_move_tree

--- knight.rb
+++ knight.rb
@@ -3,37 +3,48 @@ require_relative "tree"
 
 class KnightPathFinder 
 
+attr_reader :start_pos
 
-    def initialize(position)
-        @root_node = PolyTreeNode.new(position)
-        @board = Array.new(8) do |i|
-            Array.new(8) {|j| PolyTreeNode.new([i, j])}
-        end
-        @considered_positions = [position]
+   MOVES = [
+    [-1, -1],
+    [-1,  0],
+    [-1,  1],
+    [ 0, -1],
+    [ 0,  1],
+    [ 1, -1],
+    [ 1,  0],
+    [ 1,  1]
+  ]
+
+    def initialize(start_pos)
+        @start_pos = start_pos
+        @considered_positions = [start_pos]
 
         build_move_tree
     end
 
+
      def self.valid_moves(pos)
-         def adjacent(array, row, col)
-            rows_ndx = array.each_index.select { |i| (i-row).abs < 2 }
-            cols_ndx = array.first.size.times.select { |j| (j-col).abs < 2 }
-            rows_ndx.each_with_object([]) do |i,a| 
-            cols_ndx.each { |j| a << array[i][j] unless [i,j] == [row,col] }
-            end
-        end
+        valid_moves = []
+        
+        cur_x, cur_y = pos 
 
-        row, col = pos
-        array = @board 
+        MOVES.each do |(dx, dy)| 
+            next_pos = [cur_x + dx, cur_y + dy]
 
-        return adjacent(array, row, col)
+            if next_pos.all? {|coord| coord.between?(0, 7)}
+                valid_moves << next_pos 
+            end 
+        end 
+        
+        valid_moves
     end
 
 
     def new_move_positions(pos)
-        new_positions = []
-        
-        self.valid_moves(pos).each do |position| 
+       new_positions = []
+
+        KnightPathFinder.valid_moves(pos).each do |position| 
             if !@considered_positions.include?(position)
                 new_positions.push(position)
                 @considered_positions.push(position)
@@ -43,9 +54,33 @@ class KnightPathFinder
         new_positions
     end 
 
+    attr_accessor :root_node
 
+    def build_move_tree 
+        self.root_node = PolyTreeNode.new(start_pos)
+        nodes = [root_node]
 
-    def new_move_positions(pos)
-    end
+        until nodes.empty? 
+           
+            current_node = nodes.shift
+
+            current_pos = current_node.value
+
+            new_move_positions(current_pos).each do |new_position| 
+                new_node = PolyTreeNode.new(new_position)
+                current_node.add_child(new_node)
+                
+                nodes << new_node
+            end 
+
+        end 
+
+    end 
+
+    
     
 end 
+
+new_search = KnightPathFinder.new([1, 1])
+
+p new_search
